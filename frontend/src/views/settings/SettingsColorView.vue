c3e265ec0d9a0919502b6af4105f3934b4c4abce
Antoine Massias <massias.antoine.pro@gmail.com>
2024-08-13 17:18:05 +0200

    Made color change persistant in the browser cache.
    Changed:
    	- The website theme (aka color) is now
    		stored within the
    		browser cache (localStorage)
    		to make it persistant across
    		sessions.

--- frontend/src/views/settings/SettingsColorView.vue
+++ frontend/src/views/settings/SettingsColorView.vue
@@ -2,29 +2,22 @@
 	<div class="color_palette">
 		<div class="button_line">
 			<GlowingButton :text="'green'"
-				@click="changeGlowColor('hsl(120, 100%, 50%)', 'hsl(120, 100%, 7%)', 'sepia(100%) saturate(1000%) hue-rotate(-240deg) contrast(180%) brightness(1.2)', '#00ff00')" />
+				@click="store.commit('changeSelectedTheme', 'green')" />
 			<GlowingButton :text="'red'"
-				@click="changeGlowColor('hsl(0, 100%, 59%)', 'hsl(0, 100%, 7%)', 'sepia(100%) saturate(1000%) hue-rotate(-35deg) contrast(180%) brightness(1.2)', '#ff0000')" />
+				@click="store.commit('changeSelectedTheme', 'red')" />
 		</div>
 		<div class="button_line">
 			<GlowingButton :text="'yellow'"
-				@click="changeGlowColor('hsl(60, 100%, 50%)', 'hsl(90, 100%, 7%)', 'sepia(100%) saturate(1000%) hue-rotate(35deg) contrast(100%) brightness(1.8)', '#ffff00')" />
+				@click="store.commit('changeSelectedTheme', 'yellow')" />
 			<GlowingButton :text="'blue'"
-				@click="changeGlowColor('hsl(180, 90%, 50%)', 'hsl(180, 100%, 7%)', 'sepia(100%) saturate(1000%) hue-rotate(-210deg) contrast(120%) brightness(1.8)', '#00ffff')" />
+				@click="store.commit('changeSelectedTheme', 'blue')" />
 		</div>
 	</div>
 </template>
 
 <script setup>
 import GlowingButton from '@/components/GlowingButton.vue';
-
-const changeGlowColor = (color, b_color, logo_color, mesh_color) => {
-	document.documentElement.style.setProperty('--glow-color', color);
-	document.documentElement.style.setProperty('--background-color', b_color);
-	document.documentElement.style.setProperty('--logo-filter', logo_color);
-	document.documentElement.style.setProperty('--mesh-color', mesh_color);
-};
-
+import store from '@store';
 </script>
 
 <style scoped>
