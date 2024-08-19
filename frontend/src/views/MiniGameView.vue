4899883cf58fa39ff923c05194cca8e84bc03f74
Raven <mathisbatteux2001@gmail.com>
2024-08-19 19:50:42 +0200

    PACMAN COMMIT:
    MODIFIED:
    	-change all image color to white
    	-change the html file to a vue file
    	-implement it to the with the rest of project, to access Pacman
    	from the webiste of TRONSCENDENCE
    	-the gameplay part is finished, but it could've some
    	changes
    
    ADD:
    	-the image file in the frontend/assets/pacman_img
    	-the vue file MiniGameView.vue

--- frontend/src/views/MiniGameView.vue
+++ frontend/src/views/MiniGameView.vue
@@ -1,10 +1,52 @@
 <template>
-	<div id="game">
+	<div>
+		<p style="color: red; font-family: sans-serif; font-size: 30px">
+			<span>Score: </span><span id="scoreEl">{{ score }}</span>
+		</p>
+		<canvas ref="gameCanvas"></canvas>
 	</div>
 </template>
 
-<script setup>
+<script>
+export default {
+	data() {
+		return {
+			score: 0
+		};
+	},
+	mounted() {
+		// Lorsque le composant est monté, on initialise le canvas et le jeu
+		this.initializeCanvas();
+	},
+	methods: {
+		initializeCanvas() {
+			const canvas = this.$refs.gameCanvas;
+			const ctx = canvas.getContext('2d');
+
+			import('@assets/pacman_img/pacman.js').then(module => {
+				return module.initialize(ctx, canvas, this.updateScore);
+			}).then(() => {
+				console.log('Game initialized');
+			}).catch(error => {
+				console.error('Error initializing game:', error);
+			});
+		},
+		updateScore(newScore) {
+			this.score = newScore;
+		}
+	}
+};
 </script>
 
 <style scoped>
+body {
+	margin: 0;
+	background-color: black;
+}
+
+canvas {
+	display: block;
+	background: black;
+	margin: auto;
+}
 </style>
