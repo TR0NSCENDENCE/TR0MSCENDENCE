0129d4ffce813eee42ef707567dcd3af0d7f5521
Antoine Massias <massias.antoine.pro@gmail.com>
2024-08-19 23:42:24 +0200

    Ball now bounces on side walls

--- backend/pong/game_state.py
+++ backend/pong/game_state.py
@@ -8,19 +8,24 @@ from .models import GameInstance
 
 import math
 
+WALL_DIST = 19
+PADDLE_DIST = 35
+BALL_RADIUS = 1
+PADDLE_SIZE = 7
+
 class GameState():
     class AlreadyConnected(Exception):
         pass
 
     # Fields:
-    # p_one_connected: Boolean
-    # p_two_connected: Boolean
-    # p_one_consumer: GameConsumer
-    # p_two_consumer: GameConsumer
-    # p_one_pos: (float, float)
-    # p_two_pos: (float, float)
-    # ball_pos: (float, float)
-    # ball_vel: (float, float)
+    #   p_one_connected: Boolean
+    #   p_two_connected: Boolean
+    #   p_one_consumer: GameConsumer
+    #   p_two_consumer: GameConsumer
+    #   p_one_pos: (float, float)
+    #   p_two_pos: (float, float)
+    #   ball_pos: (float, float)
+    #   ball_vel: (float, float)
 
     def __init__(self, instance: GameInstance):
         self.instance = instance
@@ -31,7 +36,7 @@ class GameState():
         self.p_one_pos = (-35, 0)
         self.p_two_pos = (35, 0)
         self.ball_pos = (0, 0)
-        self.ball_vel = (0, 0)
+        self.ball_vel = (0.5, 0.5)
 
     def player_connect(self, player: User, consumer):
         if self.instance.player_one == player:
@@ -122,42 +127,70 @@ class GameState():
 
     TICK_RATE = 60
 
-    async def logic(self):
-        self.log("Waiting for player to disconnect")
-        while self.running():
-            await self.players_send_json({
-                'type': 'sync',
-                'state': {
-                    'ball': {
-                        'position': {
-                            'x': self.ball_pos[0],
-                            'y': self.ball_pos[1]
-                        },
-                        'velocity': {
-                            'x': self.ball_vel[0],
-                            'y': self.ball_vel[1]
-                        }
+    async def update_consumers(self):
+        await self.players_send_json({
+            'type': 'sync',
+            'state': {
+                'ball': {
+                    'position': {
+                        'x': self.ball_pos[0],
+                        'y': self.ball_pos[1]
                     },
-                    'paddle_1': {
-                        'position': {
-                            'x': self.p_one_pos[0],
-                            'y': self.p_one_pos[1]
-                        },
-                    },
-                    'paddle_2': {
-                        'position': {
-                            'x': self.p_two_pos[0],
-                            'y': self.p_two_pos[1]
-                        },
+                    'velocity': {
+                        'x': self.ball_vel[0],
+                        'y': self.ball_vel[1]
                     }
+                },
+                'paddle_1': {
+                    'position': {
+                        'x': self.p_one_pos[0],
+                        'y': self.p_one_pos[1]
+                    },
+                },
+                'paddle_2': {
+                    'position': {
+                        'x': self.p_two_pos[0],
+                        'y': self.p_two_pos[1]
+                    },
                 }
-            })
-            await asyncio.sleep(1. / self.TICK_RATE)
+            }
+        })
+
+    def update_ball_pos(self):
+        (x, y) = self.ball_pos
+        (vx, vy) = self.ball_vel
+        x += vx
+        y += vy
+        self.ball_pos = (x, y)
+
+    def handle_physics(self):
+        (bx, by) = self.ball_pos
+        (bvx, bvy) = self.ball_vel
+
+        # Ball bounce on side walls
+        if abs(by) >= WALL_DIST - BALL_RADIUS:
+            # Make ball stay within (-WALL_DIST; WALL_DIST)
+            sign = math.copysign(1, by)
+            offset = abs(by) - (WALL_DIST - BALL_RADIUS)
+            by = sign * (WALL_DIST - BALL_RADIUS - offset)
+            # Change path of ball
+            bvy = -bvy
+
+        self.ball_pos = (bx, by)
+        self.ball_vel = (bvx, bvy)
+
+    async def logic(self):
+        self.update_ball_pos()
+        self.handle_physics()
+        await self.update_consumers()
+        await asyncio.sleep(1. / self.TICK_RATE)
 
     async def game_loop(self):
         await self.wait_for_players()
         await sync_to_async(self.instance_ingame)()
-        await self.logic()
+        self.log("Waiting for player to disconnect")
+        while self.running():
+            await self.logic()
         await sync_to_async(self.instance_finished)()
         await sync_to_async(self.instance_winner)(self.p_one)
         await self.close_consumers()
