2130f8a4fac1cdbaf9e04c740aa5be87252d462e
Antoine Massias <massias.antoine.pro@gmail.com>
2024-08-21 21:23:51 +0200

    1vs1 error handling.

--- frontend/src/views/play/Multiplayer1v1View.vue
+++ frontend/src/views/play/Multiplayer1v1View.vue
@@ -1,24 +1,30 @@
 <template>
 	<div>
 		<div v-if="store.getters.isAuthenticated">
-			<div v-if="connected && !finished">
-				<GameOponentsBar
-					:player_1="players[0]"
-					:player_2="players[1]"/>
-				<PongGame
-					ref="game"
-					@onUpdateRequested="update"/>
+			<div v-if="ws_error">
+				<h1> A server error occured... </h1>
+				<GlowingButton text="main menu" dest="/" />
 			</div>
-			<GlowingText v-else :text="'Waiting for connection...'"/>
-			<MatchWon
-				v-if="finished"
-				:winner="winner"
-				:loser="loser"
-				/>
+			<div v-else-if="connected">
+				<div v-if="game_running">
+					<GameOponentsBar
+						:player_1="players[0]"
+						:player_2="players[1]"/>
+					<PongGame
+						ref="game"
+						@onUpdateRequested="update"/>
+				</div>
+				<MatchWon
+					v-else
+					:winner="winner"
+					:loser="loser"
+					/>
+			</div>
+			<h1 v-else> Waiting for connection... </h1>
 		</div>
 		<div id="must-logged" v-else>
 			<h1>You must be logged to play online.</h1>
-			<GlowingButton class="go-back-button small-button" :text="'go back'" :dest="'/play'"/>
+			<GlowingButton class="go-back-button small-button" text="go back" dest="/play'"/>
 		</div>
 	</div>
 </template>
@@ -30,7 +36,6 @@ import store from '@store';
 import { ref, onMounted, onUnmounted } from 'vue';
 import { useRoute } from 'vue-router'
 import { connectToWebsocket } from '@utils/ws';
-import GlowingText from '@components/GlowingText.vue';
 import router from '@router/index';
 import { KEYBOARD } from '@scripts/KeyboardManager';
 import { axiosInstance } from '@utils/api';
@@ -41,6 +46,7 @@ let /** @type {WebSocket} */ global_socket = undefined;
 let p1 = undefined;
 const connected = ref(false);
 const game = ref(null);
+const ws_error = ref(false)
 
 const default_player = {
 	score: 0,
@@ -59,7 +65,7 @@ const players = ref([
 ]);
 const winner = ref('');
 const loser = ref('');
-const finished = ref(false);
+const game_running = ref(true);
 
 const VELOCITY = 0.4;
 
@@ -90,9 +96,17 @@ const setup = async (/** @type {WebSocket} */ socket) => {
 	socket.onopen = () => {
 		console.log('[WS] socket connected');
 	};
-	socket.onclose = () => {
+	socket.onclose = (e) => {
 		console.log('[WS] socket closed');
+		if (!e.wasClean)
+			ws_error.value = true;
+		console.log(e)
 	};
+	socket.onerror = (e) => {
+		console.log('[WS] socket error');
+		ws_error.value = true;
+		console.log(e)
+	}
 	socket.onmessage = (e) => {
 		const event = JSON.parse(e.data);
 
@@ -127,7 +141,8 @@ const setup = async (/** @type {WebSocket} */ socket) => {
 			const winner_id = event.winner_id;
 			const has_won = store.getters.userId == winner_id;
 			const winner_index = has_won ^ inversed() ? 1 : 0;
-			finished.value = true;
+
+			game_running.value = false;
 			winner.value = players.value[winner_index].user.username;
 			loser.value = players.value[1 - winner_index].user.username;
 		}
@@ -164,15 +179,13 @@ onUnmounted(() => {
 </script>
 
 <style scoped>
-#must-logged
-{
+#must-logged {
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 }
 
-h1
-{
+h1 {
 	color: var(--glow-color);
 	margin-top: 10vh;
 	text-align: center;
