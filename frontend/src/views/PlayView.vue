bd9bb53c73d69c202fb89dfd83624ffa59865813
Lailouezzz <alanlebouder@gmail.com>
2024-08-26 15:48:04 +0200

    Removed unused view

--- frontend/src/views/PlayView.vue
+++ frontend/src/views/PlayView.vue
@@ -6,7 +6,6 @@
 import ButtonList from '@/components/ButtonList.vue';
 
 const buttons = [
-	{ text: 'solo', dest: 'play/solo', needAuth: false},
 	{ text: 'local', dest: 'play/local', needAuth: false},
 	{ text: 'multiplayer', dest: 'play/multiplayer', needAuth: true},
 	{ text: 'tournament', dest: 'play/tournament', needAuth: true},
